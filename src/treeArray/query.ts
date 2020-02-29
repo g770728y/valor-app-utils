19018ac17a234ac45594a3c5f3fafc98b222431e
g770728y <g770728y@163.com>
2020-02-29 22:05:46 +0800

    支持半选

--- src/treeArray/query.ts
+++ src/treeArray/query.ts
@@ -1,4 +1,5 @@
 import { TreeArrayItem } from ".";
+import * as R from "rambda";
 
 /**
  * i对应的节点, 可能有多个子孙节点, 返回最后一个子孙节点的index
@@ -17,4 +18,15 @@ export function getLastDecendantIndex<A extends TreeArrayItem>(
     return it.level! <= itemLevel!;
   });
   return (index < 0 ? arr.length : index + i + 1) - 1;
+}
+
+export function getDecendantIndexes<A extends TreeArrayItem>(
+  arr: A[],
+  i: number
+): number[] {
+  const itemLevel = arr[i].level;
+  const j = arr.slice(i + 1).findIndex(it => {
+    return it.level! <= itemLevel!;
+  });
+  return j < 0 ? R.range(i + 1, arr.length) : R.range(i + 1, i + j + 1);
 }
